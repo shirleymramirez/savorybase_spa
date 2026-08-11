38bb53ff99a4915334deb49388f28f2959290ee3
Shirley <shirley.mramirez@yahoo.com>
2026-08-11 13:30:02 -0700

    resolved build error for dynamic route and added production-sage fallback page for issing food slugs

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      },
-      {
-        protocol: "http",
-        hostname: "**"
-      }
-    ]
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
